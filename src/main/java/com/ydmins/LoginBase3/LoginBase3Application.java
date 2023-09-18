bd10b9e2ddfc7a5d96e7585d7143556ff86a715a
figulr <dechallintier@gmail.com>
2023-09-18 12:10:47 +0900

    first commit

---
+++ src/main/java/com/ydmins/LoginBase3/LoginBase3Application.java
@@ -0,0 +1,13 @@
+package com.ydmins.LoginBase3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginBase3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginBase3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ydmins/LoginBase3/LoginBase3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ydmins.LoginBase3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginBase3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
